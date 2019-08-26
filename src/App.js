83c49a45ad0a9494d15adddfee870463040cbeee
smurzak <lordarkon1@gmail.com>
2019-08-26 19:58:11 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+//import Student from './students/students';
+//import Post from './students/Post';
+//import UsersList from './users/users.controller';
+import Home from './home/Home';
+import UsersList from './users/users.controller';
+import Documentation from './documentation/Documentation';
+import UserInfo from './userinfo/UserInfo';
 
-function App() {
+
+// const post = {
+//   author: "Yevheniia Kyryliuk",
+//   title: "Week #11 (Mon) - React app (styled-components)",
+//   image: "https://scontent.fhrk2-1.fna.fbcdn.net/v/t1.0-9/67779841_2425357304453751_8147511213429882880_n.jpg?_nc_cat=105&_nc_oc=AQmwacDeBYo_RelqMGktGr0twLE5YMhJnZ9iM4y2_zS55xvYMIRo8gTUGXVrnz9CpsY&_nc_ht=scontent.fhrk2-1.fna&oh=013eab63c543d8488d0e3dee8c84bad6&oe=5DD28C61"
+// }
+
+const App = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Router>
+      <Route path = '/' component = {Home} />
+      <Route path = '/userslist' component = {UsersList} />
+      <Route path = '/user/:id' component = {UserInfo} />
+      <Route path = '/documentation' component = {Documentation} />
+    </Router>
+)};
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+
+
+ // document.createElement, VirtualDom
+
+// <div class="red">
+//   <a href="#">About</a>
+// </div>
+//React.createElement("div", {className: "red"}, React.createElement("a", {href: "#"}, "About"))
+
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.createPortal(<div>Hello team!</div>, document.getElementById('root2'));
+
+
+
+
+
+
+
+
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
